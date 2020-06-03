ea3e888d8c5b9411db98c85aa3b2ba7c1499eafd
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-06-03 19:27:19 -0400

    CORS enabled. Projects controller added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,26 +2,44 @@ class ProjectsController < ApplicationController
 
     def index 
         projects = Project.all 
+        # this should be limited to an id
         render json: ProjectSerializer.new(projects)
     end 
 
     def create 
+        project = Project.create(project_params)
+        project.save
+        render json: project
     end 
 
     def show
+        find_project
+        render json: ProjectSerializer.new(project)
     end 
 
     def update 
-        project = Project.find_by(id: params[:id])
-        # project.update(complete: params[:complete])
+        find_project
+        project.update(project_params)
         project.save
         render json: TaskSerializer.new(project)
     end 
 
     def destroy
-        project = Project.find_by(id: params[:id])
+        find_project
         project.destroy
         render json: project
     end 
+
+    private 
+
+    def find_project
+        project = Project.find_by(id: params[:id])
+    end
+
+    def project
+        params.require(:project).permit(:title, :description, :due_date, :status, :notes,
+        user_attributes: [:id]
+        )
+    end
 
 end
