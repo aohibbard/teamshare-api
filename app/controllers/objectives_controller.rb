5f6236ed56bc3f8a26ab2f4fd657a07951c3b779
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-06 13:45:32 -0400

    Clarify objective_params, update name

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -2,13 +2,15 @@ class ObjectivesController < ApplicationController
 
     def index
         objectives = Objective.all
+        # should objectives be nested under projects?
+        # @objectives = @project.objectives.all
         render json: ObjectiveSerializer.new(objectives)
     end
 
     def create
         objective = Objective.create(objective_params)
         objective.save
-        render json: objective
+        render json: @objective
     end
 
     def show
@@ -25,7 +27,13 @@ class ObjectivesController < ApplicationController
 
     private
 
-    def objective 
+    def find_objective
+        objective = Objective.find_by(id: params[:id])
+        # or this? are objectives nested under projects?
+        # @objective = @project.objectives.find_by(id: params[:id])
+    end
+
+    def objective_params 
         params.require(:objective).permit(:title, :status, :notes)
     end
 
