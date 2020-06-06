d03dd0f51d766675e45b1d93283cab9104b58a2d
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-06 12:46:52 -0400

    Stub out objectives controller routes

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,2 +1,29 @@
 class ObjectivesController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def objective 
+
+    end
+    
 end
