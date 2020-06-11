5167116a44f466aa57db5fceb8ee18bd6f23690f
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-06-11 16:09:25 -0400

    adds seed data. fixes error in Objectives model relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 10.times do
-    User.create(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, email: Faker::Internet.unique.email, password: "password")
+    User.create(first_name: Faker::Name.unique.first_name, last_name: Faker::Name.unique.last_name, email: Faker::Internet.unique.email, password_digest: "password")
 end 
-# password: Faker::Internet.password(min_length: 5, max_length: 10, mix_case: true
+password: Faker::Internet.password(min_length: 5, max_length: 10, mix_case: true
 
 
-10
+10.times do 
+    Project.create(title: Faker::Lorem.sentence(word_count: 2), description: Faker::Lorem.sentence(word_count: 9), due_date: Faker::Date.in_date_period(year: 2020, month: 9), status: "incomplete", notes: Faker::Lorem.sentences(number: 3))
+end 
+
+45.times do 
+    Objective.create(title: Faker::Lorem.sentence(word_count: 3), status: "incomplete", notes: Faker::Lorem.sentences(number: 3), projects_id: Random.new.rand(1..11))
+end 
