b7eb4a3c5550bed8ee8ad9e39bd335aec5fb6fbc
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-24 05:56:34 -0400

    Allow any origin for Rack Cors - replace template address with * to allow all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
+# Uncomment this chunk below
+# Add origin * to allow for all origins
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
 
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
