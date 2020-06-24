6fe601c3ea34a599562bf8b71c19bdc2a7c37b8b
AnnaWijetunga <annawijetunga@gmail.com>
2020-06-24 06:06:06 -0400

    Add * to allow all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,17 +5,11 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-<<<<<<< HEAD
 # Uncomment this chunk below
 # Add origin * to allow for all origins
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-=======
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'example.com'
->>>>>>> 6a5efc32d6bd901d66035ed937996139a44c30e7
 
     resource '*',
       headers: :any,
