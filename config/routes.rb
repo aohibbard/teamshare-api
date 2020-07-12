0a0c726e1d8305338f03a1a3507057f3817a20f8
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-07-12 12:54:08 -0400

    adds login/out routes. nests users route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   scope :api do 
-    resources :projects do 
-      resources :objectives
-    end 
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  resources :users do
+      resources :projects do 
+        resources :objectives
+      end 
+    end
   end
 
   # everything should eventually be nested under users, with backend controls for security, yes?
