19975362eabd2b2e99102937c4512994b60d1683
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-07-12 12:26:30 -0400

    adds validations & full_name method on user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
-    has_secure_password
-
     has_and_belongs_to_many :projects
     has_many :objectives, :through => :projects
 
-    def name
-        self.first_name + self.last_name
+    has_secure_password
+
+    # validations
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+    def full_name
+        self.first_name + ' ' + self.last_name
     end 
     
 end
